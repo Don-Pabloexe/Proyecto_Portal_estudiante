ec8a462374f91aebcca12168e0a2497b3e1126a9
Sebastian Muñoz Riquelme <112529007+JanBoseyur@users.noreply.github.com>
2022-09-17 11:28:25 -0300

    a

--- Avance_Sebastian/resources/views/welcome.blade.php
+++ Avance_Sebastian/resources/views/welcome.blade.php
@@ -18,15 +18,47 @@
             }
 
             .navbar {
-                padding-top: 22px;
-                padding-bottom: 22px;
+                position: -webkit-sticky;
+                position: sticky;
+                top: 0;
+                padding-top: 18px;
+                padding-bottom: 18px;
+                
+            }
+
+            .navbar2 {
+                position: -webkit-sticky;
+                position: sticky;
+                display: block;
+                top: 0;
+                width: 28px;
+                height: 28px;
                 margin-left: 10px;
-                margin-right: 10px;
+                
             }
 
             .nav-r{
+                position: sticky;
+                text-align: center;
+            }
+
+            .nav-r2{
                 float: right;
                 text-align: right;
+                position: sticky;
+                margin-left: 10px;
+            }
+
+
+            .secciones1 {
+                margin-top: 50px;
+                display: inline;
+            }
+
+            .secciones2 {
+                width: 50%;
+                border: solid 2px;
+
             }
 
         </style>
@@ -39,11 +71,19 @@
             <img href = "#" src = "{{ asset('images/icon_uct2.png') }}" alt = "" width = "130" height = "45" aria-controls = "sidebar">
 
             <div>
-                <img href = "#" src = "{{ asset('images/icon_uct.png') }}" class = "nav-r" width = "25%">
-                <img href = "#" src = "{{ asset('images/icon_uct.png') }}" class = "nav-r" width = "25%">
-                <img href = "#" src = "{{ asset('images/icon_uct.png') }}" class = "nav-r" width = "25%">
+                <img href = "#" src = "{{ asset('images/logout.png') }}" class = "nav-r" width = "35">
+            </div>
+
+            <div>
+                <img href = "#" src = "{{ asset('images/fb.png') }}" class = "nav-r2" width = "35">
+                <img href = "#" src = "{{ asset('images/ig.png') }}" class = "nav-r2" width = "35">
             </div>
         </nav>
+
+        <div class = "secciones1">
+            <img href = "#" src = "{{ asset('images/cursos.jpg') }}" class = "secciones2">
+            <img href = "#" src = "{{ asset('images/cursos.jpg') }}" class = "secciones2">
+        </div>
 
         
         <script src = "{{ asset('js/app.js') }}" type = "text/js"></script>
