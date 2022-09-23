5e8e26c0d0eaee4ce391415ce5a8617ec0f23210
Rick <ricknavarrete16@gmail.com>
2022-09-22 23:57:12 -0300

    Avance_Menu

---
+++ example-app/resources/views/welcome.blade.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+        <!-- Styles -->
+        <style>
+            body {
+                background-image: url("{{ asset('45879.jpg') }} ");
+                background-attachment: fixed;
+                background-repeat: no-repeat;
+                background-size: cover; 
+            }
+
+        </style>
+    </head>
+    <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <!-- Logo de la universidad que al presionarlo lleva a la paguina oficial -->
+        <div class="container-fluid">
+            <a class="navbar-brand" href="https://www.uct.cl/%22%3E">
+                <img src="Universidad_Catolica_de_Temuco_Logo_Vertical.png" width="150" height="50" class="d-inline-block align-top" alt="">
+            </a>      
+        </div>
+        <!-- acceso a las rede sociales de la Universidad -->
+            <div class="d-grid gap-2 d-md-block col-1">
+                <a href = "https://www.facebook.com/canaluctemuco/"><img href = "#" src = "{{ asset('fb.png') }}" class = "nav-r2" width = "32"></a>
+                <a href = "https://www.instagram.com/uctemuco/?hl=es-la"><img href = "#" src = "{{ asset('ig.png') }}" class = "nav-r2" width = "32"></a>
+                <a href = "#"><img href = "#" src = "{{ asset('logout.png') }}" class = "nav-r2 ml-4" width = "34"></a>
+            </div>
+    </nav>
+    <!-- -->
+    <h1 style = "text-align: center;">Bienvenido PABLO</h1>
+
+    <div class="text-center">
+        <img src="carp_colo2.png" width="300" height="300" class="img-thumbnail" alt="...">
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-8 text-center">
+            <ul class="list-group">
+                <li class="list-group-item">Rut: 19.807.541-k</li>
+                <li class="list-group-item">Nombres: Pablo Javier </li>
+                <li class="list-group-item">Apellido paterno: Gomez</li>
+                <li class="list-group-item">Apellido materno: Quidel</li>
+                <li class="list-group-item">Fecha de Nacimiento: 03-03-2003</li>
+                <li class="list-group-item">Sexo: Masculino</li>
+                <li class="list-group-item">E-Mail UCTemuco: Pgomez2021@uct.cl</li>
+    </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="row mt-5 justify-content-around align-items-center">
+        <div class="col-3">
+            <div class="card" >
+            <div class="card-body">
+                <h5 class="card-title">Solicitud Nota P</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">ir</a>
+            </div>
+        </div>
+    </div>
+
+        <div class="col-3">
+            <div class="card" >
+            <div class="card-body">
+                <h5 class="card-title">Prestamo Biblioteca</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">ir</a>
+            </div>
+        </div>
+    </div>
+
+        <div class="col-3">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Informacion Academica</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">ir</a>
+            </div>
+        </div>
+    </div>
+
+    </div>
+
+    <div class="row mt-5 justify-content-around align-items-center">
+        <div class="col-3">
+            <div class="card" >
+            <div class="card-body">
+                <h5 class="card-title">Notas Parciales</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">ir</a>
+            </div>
+        </div>
+    </div>
+
+        <div class="col-3">
+            <div class="card" >
+            <div class="card-body">
+                <h5 class="card-title">Cuenta Corriente</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">ir</a>
+            </div>
+        </div>
+    </div>
+
+        <div class="col-3">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Horas con la Asistente social</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">ir</a>
+            </div>
+        </div>
+    </div>
+
+    </div>
+    <div class="row mt-5 justify-content-around align-items-center">
+        <div class="col-3">
+            <div class="card" >
+            <div class="card-body">
+                <h5 class="card-title">Documentos</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">ir</a>
+            </div>
+        </div>
+    </div>
+
+        <div class="col-3">
+            <div class="card" >
+            <div class="card-body">
+                <h5 class="card-title">Solicitudes Estudiantiles</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">ir</a>
+            </div>
+        </div>
+    </div>
+
+        <div class="col-3">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Observacione Ficha Academica</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">ir</a>
+            </div>
+        </div>
+    </div>
+
+    </div>
+
+    <div class="row mt-5 justify-content-around align-items-center">
+        <div class="col-3">
+            <div class="card" >
+            <div class="card-body">
+                <h5 class="card-title">Certificado Academico</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">ir</a>
+            </div>
+        </div>
+    </div>
+
+                </div>
+            </div>
+        </div>
+    <!-- footer -->
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <section>
+            <footer class="text-center text-lg-start bg-white text-muted" id="footer1" >
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom" id="footer1">
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block ">
+        <h4 class="text-dark">Datos Importantes:</h4>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <div id="footer3">
+        <a href="" class="me-4 link-secondary">
+            <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="" class="me-4 link-secondary">
+            <i class="fab fa-twitter"></i>
+        </a>
+        <a href="" class="me-4 link-secondary">
+            <i class="fab fa-google"></i>
+        </a>
+        <a href="" class="me-4 link-secondary">
+            <i class="fab fa-instagram"></i>
+        </a>
+        <a href="" class="me-4 link-secondary">
+            <i class="fab fa-linkedin"></i>
+        </a>
+        <a href="" class="me-4 link-secondary">
+            <i class="fab fa-github"></i>
+        </a>
+    </div>
+    <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
+    <section class="footer3">
+        <div class="container text-center text-md-start mt-5">
+    <!-- Grid row -->
+        <div class="row mt-3">
+    <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+    <!-- Content -->
+            <h6 class="text-uppercase fw-bold mb-4 text-dark">
+                <i class="fas fa-gem me-3 text-secondary "></i>Titulo
+            </h6>
+            <p class="text-dark">
+                Pagina oficial Universidad Catolica de temuco
+            </p>
+        </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+    <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4 text-dark">
+              DIRECCIONES CAMPUS
+            </h6>
+            <p class="text-decoration-underline text-dark fw-semibold">CAMPUS SAN FRANCISCO</p>
+            <p class="text-dark">
+                Calle: Manuel Montt 56
+                Fono: +56 45 2 205 470
+            </p>
+            <p class="text-decoration-underline text-dark fw-semibold">CAMPUS SAN JUAN PABLO II</p>
+            <p class="text-dark">
+                Rudecindo Ortega 02950
+                Fono: +56 45 2 553 978
+            </p>
+          </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+    <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4 text-dark">
+              TELÉFONOS DE UTILIDAD
+            </h6>
+            <p class="text-decoration-underline text-dark fw-semibold">BIENESTAR ESTUDIANTIL</p>
+            <p class="text-dark">
+                Calle: Manuel Montt 56
+                Fono: +56 45 2 205 424
+            </p>
+           <p class="text-decoration-underline text-dark fw-semibold">PRENSA INSTITUCIONAL</p>
+            <p class="text-dark">
+              Avenida Alemania 0211
+              Fono: +56 45 2 205 428
+            </p>
+          </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+    <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4 text-dark">CORREOS</h6>
+           <p class="text-decoration-underline text-dark fw-semibold">Oficina de Informaciones</p>
+            <p class="text-dark">
+              Informaciones: info@uct.cl
+            </p>
+            <p class="text-decoration-underline text-dark fw-semibold">Soporte Sistemas</p>
+            <p class="text-dark">
+              soportesistemas@uct.cl
+            </p>
+            <p class="text-decoration-underline text-dark fw-semibold">Becas y Créditos Beneficios Estudiantiles</p>
+            <p class="text-dark">
+              bienestarestudiantil@uct.cl
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div class="text-center p-4" id="footer2">
+    Portal del Estudiante es un proyecto realizado por la Dirección de Desarrollo de Sistemas UCTemuco.
+    Soporte, comentarios, errores reportarlos al correo: soportesistemas@uct.cl.
+    </div>
+ 
+  </footer>
+  </section>
+        
+    </body>
+</html>
